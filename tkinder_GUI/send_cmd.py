04ff284dbbed7101f8b5ca6778a6de9097adbca2
PRAVEEN_BOSCO <boscopraveen77@gmail.com>
2024-07-15 12:03:25 +0530

    Changes to be committed: 	new file:   UDP/server 	modified:   tkinder_GUI/send_cmd.py

--- tkinder_GUI/send_cmd.py
+++ tkinder_GUI/send_cmd.py
@@ -1,17 +1,44 @@
+# import socket
+# import time
+# import struct
+# import threading
+# import sys
+
+# def main():
+#     s=socket.socket()
+#     s.connect(("127.0.0.1", 3003))
+
+#     cmd = struct.pack("=BBIBBBB", 9, 2, 1000, 49, int(sys.argv[1]), 57, int(sys.argv[2]))
+#     s.send(cmd)
+    
+#     s.close()
 import socket
-import time
 import struct
-import threading
 import sys
 
 def main():
-    s=socket.socket()
-    s.connect(("127.0.0.1", 3003))
+    if len(sys.argv) != 3:
+        print("Usage: script.py <arg1> <arg2>")
+        sys.exit(1)
 
-    cmd = struct.pack("=BBIBBBB", 9, 2, 1000, 35, int(sys.argv[1]), 36, int(sys.argv[2]))
-    s.send(cmd)
-    
-    s.close()
+    try:
+        arg1 = int(sys.argv[1])
+        arg2 = int(sys.argv[2])
+    except ValueError:
+        print("Arguments must be integers")
+        sys.exit(1)
 
+    try:
+        with socket.socket() as s:
+            s.connect(("127.0.0.1", 3003))
+            cmd = struct.pack("=BBIBBBB", 9, 2, 1000, 35, arg1, 36, arg2)
+            s.send(cmd)
+    except socket.error as e:
+        print(f"Socket error: {e}")
+        sys.exit(1)
+    except struct.error as e:
+        print(f"Struct packing error: {e}")
+        sys.exit(1)
 
-main()
+if __name__ == "__main__":
+    main()
